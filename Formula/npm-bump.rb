d8dbf9f412e40301b2e78eeeecec7cea3aba694d
Vincent Hardouin <vincenthardouin@icloud.com>
2022-11-06 12:23:57 +0100

    npm-bump 0.6.0

--- Formula/npm-bump.rb
+++ Formula/npm-bump.rb
@@ -1,8 +1,8 @@
 class NpmBump < Formula
   desc "It's a bash script for bump npm packages"
   homepage "https://github.com/VincentHardouin/npm-bump"
-  url "https://github.com/VincentHardouin/npm-bump/archive/v0.5.1.tar.gz"
-  sha256 "12d0631599bb414754062d5d3fe3b6afd35f37e9ecf1073eac4a4623dc061e4e"
+  url "https://github.com/VincentHardouin/npm-bump/archive/v0.6.0.tar.gz"
+  sha256 "e59be2b8d40c79d6b0d51a7231719f9971f1f68dfb7eaf29637b79f8396d7589"
   license "AGPL-3.0-or-later"
 
   def install
